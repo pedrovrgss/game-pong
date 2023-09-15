8dc6bebc88ffcba2bb10ec459044f3dc218436ec
Pedro Vargas - Laptop <phvs2005@outlook.com>
2023-09-15 10:49:15 -0300

    segunda entrega

--- project.py
+++ project.py
@@ -1,6 +1,28 @@
 from PPlay.window import *
+from PPlay.sprite import Sprite
 
-while (True):
-    janela = Window(400, 400)
-    janela.set_title("Pedro Vargas")
-    janela.set_background_color((255, 255, 255))
+# Cria uma janela
+janela = Window(800, 600)
+janela.set_title("Pedro Vargas")
+
+# Define a cor de fundo como branco (255, 255, 255)
+janela.set_background_color((0, 0, 0))
+
+while True:
+    # Loop do jogo aqui
+
+    # Cria uma bolinha de 40x40 pixels no centro da tela
+    bolinha = Sprite("bolinha.png")
+    bolinha.x = janela.width / 2 - bolinha.width / 2
+    bolinha.y = janela.height / 2 - bolinha.height / 2
+
+    # Desenha a bolinha na tela
+    bolinha.draw()
+
+    janela.update()
+
+    if janela.get_keyboard().key_pressed("ESC"):
+        break
+
+janela.close()
+
